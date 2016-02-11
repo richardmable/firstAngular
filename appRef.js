117ef6adf190bceb35b3ebba4db03115a3ae86c4
Richard Mable <richardbmable@gmail.com>
2016-02-11 16:12:01 -0500

    page with routing now displays, working on getting comments to display

--- appRef.js
+++ appRef.js
@@ -7,41 +7,88 @@ angular.module('flapperNews', ['ui.router'])
 // this is the Angular config function to setup a home state
 // otherwise() redirects to unspecified routes
 .config([
-	'stateProvider',
-	'urlRouterProvider',
+	'$stateProvider',
+	'$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
 		$stateProvider
 			// the state is given a name
 			.state('home', {
 				// the state is given a url
-				url: '/home'
+				url: '/home',
 				// and a template
-				templateUrl: '/home.html'
+				templateUrl: '/home.html',
 				// and should be controlled by MainCtrl
 				controller: 'MainCtrl'
-		});
+			})
+			// the posts state configuration
+			.state('posts', {
+				// brackets around id to indicate it is a route parameter
+				url: 'posts/{id}',
+				templateUrl: '/posts.html',
+				controller: 'PostsCtrl'
+			})
+
 		$urlRouterProvider.otherwise('home');
-	}]);
+	}])
 
 // this is a service 
 // You may be wondering why we're using the keyword factory instead of service. 
 // In angular, factory and service are related in that they are both instances of a third entity called provider.
 .factory('posts', [function(){
 	var o = {
-		posts: []
+		posts: [{title: 'A new post!', link: "www.google.com", upvotes: 0}]
 	};
 	return o;
 
-}]);
+}])
 
 .controller('MainCtrl', [
 	'$scope',
 	//this injects posts service into the main controller
-	'posts'
+	'posts',
+
 	function($scope, posts){
 		//can only have two way data binding with $scope variables
 		//this line mirrors the array returned by the service
 		//this way, we can display the posts to the view
 		$scope.posts = posts.posts;
+		// the add posts function
+		$scope.addPost = function(){
+			$scope.posts.push({
+				title: $scope.title,
+				link: $scope.link,
+				upvotes: 0,
+				comments: [
+					{author: 'Joe', body: 'Cool post!', upvotes: 0},
+    				{author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+				]
+			})
+		};
+		$scope.addComment = function(){
+			if($scope.body === '') { return; }
+  			$scope.post.comments.push({
+    			body: $scope.body,
+    			author: 'user',
+    			upvotes: 0
+  		});
+  			$scope.body = '';
+		};
+		$scope.incrementUpvotes = function(post){
+			// increment that post's upvotes by 1
+			post.upvotes += 1;
+		};
 
-}]);
+}])
+
+.controller('PostsCtrl', [
+	'$scope',
+	// inject our state params to make sure we are sending the params for the post
+	'$stateParams',
+	// inject the posts service into the posts controller
+	'posts',
+	function($scope, $stateParams, posts){
+		// sets a scope object called post that grabs the appropriate post from 
+		// from the posts service using $stateParams
+		$scope.post = posts.posts[$stateParams.id];
+
+	}]);
